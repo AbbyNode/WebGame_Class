ad40f87c226942e1490a85573b91bd83628cae17
Abby <AbbyNode@users.noreply.github.com>
2020-02-08 11:05:33 -0500

    Scenes

--- Scripts/objects/Scene.ts
+++ Scripts/objects/Scene.ts
@@ -1,5 +1,25 @@
 module objects {
     export abstract class Scene extends createjs.Container {
-        
+        constructor() {
+            super();
+        }
+
+        /**
+         * Initialization method
+         *
+         * @abstract
+         * @memberof Scene
+         */
+        public abstract Start():void;
+
+        /**
+         * Update method
+         *
+         * @abstract
+         * @memberof Scene
+         */
+        public abstract Update():void;
+
+
     }
 }
---
+++ Scripts/scenes/Start.ts
@@ -0,0 +1,9 @@
+module scenes {
+    export class Start extends objects.Scene {
+        public Start(): void {
+            throw new Error("Method not implemented.");
+        }        public Update(): void {
+            throw new Error("Method not implemented.");
+        }
+    }
+}
